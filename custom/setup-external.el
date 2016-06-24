b61da661cbfb3a44cac4501210b13199228ebdbb
xiaomengbai <xiaomengbai@gmail.com>
2016-06-24 13:21:23 -0700

    comment out dictionary part

--- custom/setup-external.el
+++ custom/setup-external.el
@@ -89,35 +89,35 @@
 (require 'shell-pop)
 (global-set-key (kbd "C-c t") 'shell-pop)
 
-(require 'sdcv)
-(require 'showtip)
-(setq sdcv-dictionary-simple-list        ;; a simple dictionary list
-      '(
-        ;; "懒虫简明英汉词典"
-        "懒虫简明汉英词典"
-        ;; "KDic11万英汉词典"
-        ;; "牛津现代英汉双解词典"
-        "朗道英汉字典5.0"
-        ))
-(setq sdcv-dictionary-complete-list      ;; a complete dictionary list
-      '(
-        ;; "KDic11万英汉词典"
-        ;; "懒虫简明英汉词典"
-        ;; "朗道英汉字典5.0"
-        ;; "XDICT英汉辞典"
-        ;; "朗道汉英字典5.0"
-        ;; "XDICT汉英辞典"
-        "懒虫简明汉英词典"
-        ;; "牛津英汉双解美化版"
-        ;; "stardict1.3英汉辞典"
-        ;; "英汉汉英专业词典"
-        ;; "CDICT5英汉辞典"
-        ;; "Jargon"
-        ;; "FOLDOC"
-        ;; "WordNet"
-        "牛津现代英汉双解词典"
-        "朗道英汉字典5.0"
-        ))
+;; (require 'sdcv)
+;; (require 'showtip)
+;; (setq sdcv-dictionary-simple-list        ;; a simple dictionary list
+;;       '(
+;;         ;; "懒虫简明英汉词典"
+;;         "懒虫简明汉英词典"
+;;         ;; "KDic11万英汉词典"
+;;         ;; "牛津现代英汉双解词典"
+;;         "朗道英汉字典5.0"
+;;         ))
+;; (setq sdcv-dictionary-complete-list      ;; a complete dictionary list
+;;       '(
+;;         ;; "KDic11万英汉词典"
+;;         ;; "懒虫简明英汉词典"
+;;         ;; "朗道英汉字典5.0"
+;;         ;; "XDICT英汉辞典"
+;;         ;; "朗道汉英字典5.0"
+;;         ;; "XDICT汉英辞典"
+;;         "懒虫简明汉英词典"
+;;         ;; "牛津英汉双解美化版"
+;;         ;; "stardict1.3英汉辞典"
+;;         ;; "英汉汉英专业词典"
+;;         ;; "CDICT5英汉辞典"
+;;         ;; "Jargon"
+;;         ;; "FOLDOC"
+;;         ;; "WordNet"
+;;         "牛津现代英汉双解词典"
+;;         "朗道英汉字典5.0"
+;;         ))
 
-(global-set-key (kbd "C-c ?") 'sdcv-search-pointer+)
-(global-set-key (kbd "C-c C-?") 'sdcv-search-pointer)
+;; (global-set-key (kbd "C-c ?") 'sdcv-search-pointer+)
+;; (global-set-key (kbd "C-c C-?") 'sdcv-search-pointer)
--- init.el
+++ init.el
@@ -93,10 +93,21 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
- '(custom-safe-themes (quote ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+ '(custom-safe-themes
+   (quote
+    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
  '(helm-mode t)
  '(org-agenda-files (quote ("~/projects/note/todo.org")))
- '(tex-dvi-view-command (quote (cond ((eq window-system (quote x)) "emacsclient -e '(progn (find-file-other-window \"*\"))'") ((eq window-system (quote w32)) "yap") (t "dvi2tty * | cat -s")))))
+ '(tex-dvi-view-command
+   (quote
+    (cond
+     ((eq window-system
+          (quote x))
+      "emacsclient -e '(progn (find-file-other-window \"*\"))'")
+     ((eq window-system
+          (quote w32))
+      "yap")
+     (t "dvi2tty * | cat -s")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
