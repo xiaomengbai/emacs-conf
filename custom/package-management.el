cb06ac6e55e162c9ce8795c7fd37038cc75f8bc7
xiaomengbai <xiaomengbai@gmail.com>
2022-05-18 06:22:13 +0800

    add package source in China

--- custom/package-management.el
+++ custom/package-management.el
@@ -1,9 +1,14 @@
 ;; we load "package" and "use-package" in this module
 (message "Loading package management packages...")
 (require 'package)
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
+(setq package-archives '(
+                         ;("melpa" . "https://melpa.org/packages/")
+                         ("melpa" . "https://elpa.zilongshanren.com/melpa/")
+			 ;("org" . "https://orgmode.org/elpa/")
+                         ("org" . "https://elpa.zilongshanren.com/org/")
+                         ;;("gnu" . "https://mirror.iscas.ac.cn/elpa/")
+                         ("gnu" . "https://elpa.zilongshanren.com/gnu/")
+			 ))
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
